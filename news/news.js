9f04a8c09d7186fbaed1a9415009147bcf0a48d8
JacobLinCool <jacoblin2011@gmail.com>
2019-02-12 18:46:44 +0800

    Update news.js

--- news/news.js
+++ news/news.js
@@ -1,7 +1,7 @@
 var nlll = "pqlau"; // News Lists List Location
 var nll = xhr.json("https://api.myjson.com/bins/"+nlll);
-var nl = getList(nll[0]);
-for(var i = 0; i < nl.length; i++) {
+var nl = getList(nll[nll.length-1]);
+for(var i = nl.length-1; i >= 0; i--) {
     var n = getNews(nl[i]);
 }
 
@@ -10,4 +10,7 @@ function getList(loc) {
 }
 function getNews(loc) {
     return xhr.json("https://api.myjson.com/bins/"+loc);
+}
+function drawNews() {
+    
 }
