0144746edd73afab6d3834c06a46121b623ebd6e
JacobLinCool <jacoblin2011@gmail.com>
2019-02-12 18:20:53 +0800

    Update news.js

--- news/news.js
+++ news/news.js
@@ -1,6 +1,13 @@
-var nlll = ""; // News Lists List Location
+var nlll = "pqlau"; // News Lists List Location
 var nll = xhr.json("https://api.myjson.com/bins/"+nlll);
+var nl = getList(nll[0]);
+for(var i = 0; i < nl.length; i++) {
+    var n = getNews(nl[i]);
+}
 
 function getList(loc) {
-    
+    return xhr.json("https://api.myjson.com/bins/"+loc);
+}
+function getNews(loc) {
+    return xhr.json("https://api.myjson.com/bins/"+loc);
 }
