66ae1578dceebedc29a6ee487de7aafd98ff997a
JacobLinCool <jacoblin2011@gmail.com>
2019-02-25 12:41:17 +0800

    Update review.js

--- So-Hateful/review.js
+++ So-Hateful/review.js
@@ -1,6 +1,6 @@
 function reviewSub(tar, dic) {
     for(var i = 0; i < dic.length; i++) {
-        if(tar == dic[i]) {
+        if(tar.includes(dic[i])) {
             return false;
         }
     }
