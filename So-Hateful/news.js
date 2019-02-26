b996f64498fc3873dc33943547552a11a2f6243d
JacobLinCool <jacoblin2011@gmail.com>
2019-02-26 09:59:40 +0800

    Update news.js

--- So-Hateful/news.js
+++ So-Hateful/news.js
@@ -1,4 +1,3 @@
-drawNews("歡迎投稿", "<h3>歡迎任何人投稿: <a href='javascript:location.href=\"submit\"' style='color: blue'>我要投稿！</a></h3>", "ADMIN", "置頂文章");
 var p = ((urldata("p")!="://1481.tk/So-Hateful/") ? urldata("p") : 1);
 var nlll = "pqlau"; // News Lists List Location
 var nll = xhr.json("https://api.myjson.com/bins/"+nlll);
@@ -33,6 +32,7 @@ function drawNews(title, content, author, time) {
     newsBlock.appendChild(newsDiv);
 }
 function reloadNews() {
+    drawNews("歡迎投稿", "<h3>歡迎任何人投稿: <a href='javascript:location.href=\"submit\"' style='color: blue'>我要投稿！</a></h3>", "ADMIN", "置頂文章");
     document.getElementById("newsBlock").innerHTML = "";
     for(var i = nl.length-(1+6*(p-1)); i >= nl.length-(1+6*p); i--) {
         if(i < 0) {break;}
