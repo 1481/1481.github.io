8889ad6ed290bca18a0212cdcb0121d632fc0941
JacobLinCool <jacoblin2011@gmail.com>
2019-03-01 20:05:28 +0800

    Update some.js

--- js/some.js
+++ js/some.js
@@ -3,6 +3,7 @@ loadingMsg.style.display = "none";
 loadingMsg.style.position = "fixed";
 loadingMsg.style.top = "0.1rem";
 loadingMsg.style.left = "0.1rem";
+loadingMsg.style.zIndex = "9999";
 loadingMsg.innerHTML = "Loading...";
 document.body.appendChild(loadingMsg);
 window.addEventListener("pageshow", function() {loadingMsg.style.display = "none";});
