2f6be8769636a9aa7e6c2497da6f8ac067821ddd
JacobLinCool <jacoblin2011@gmail.com>
2019-03-09 18:05:12 +0800

    Update convertURL.js

--- So-Hateful/convertURL.js
+++ So-Hateful/convertURL.js
@@ -1,7 +1,7 @@
 function convertURL(tar) {
 	  var text=tar.value;
 	  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-	  var text1=text.replace(exp, "<a href='$1'>$1</a>");
+	  var text1=text.replace(exp, "<a target='_blank' href='$1'>$1</a>");
 	  var exp2 =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
 	  return text1.replace(exp2, '$1<a target="_blank" href="http://$2">$2</a>');
 }
