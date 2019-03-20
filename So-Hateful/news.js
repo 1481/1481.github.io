37cd6a31e1f7f54863bd7f2b628a44747c072af7
JacobLinCool <jacoblin2011@gmail.com>
2019-03-20 18:30:40 +0800

    Update news.js

--- So-Hateful/news.js
+++ So-Hateful/news.js
@@ -53,7 +53,7 @@ function drawNews(title, content, author, time, n = {}, id = "", number = 0) {
 }
 function reloadNews() {
     document.getElementById("newsBlock").innerHTML = "";
-    drawNews("歡迎投稿", "<h3>歡迎任何人投稿: <a href='javascript:location.href=\"submit\"' style='color: blue'>我要投稿！</a></h3>", "ADMIN", "置頂文章");
+    drawNews("歡迎投稿", "<h3>歡迎任何人投稿: <a href='javascript:location.href=\"submit\";setTimeout(function(){elm.id(\'comment-div\').style.display=\'none\'}, 5);' style='color: blue !important'>我要投稿！</a></h3>", "ADMIN", "置頂文章");
     for(var i = nl.length-(1+6*(p-1)); i >= nl.length-(1+6*p); i--) {
         if(i < 0) {break;}
         var n = getNews(nl[i]);
