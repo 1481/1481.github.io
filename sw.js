4a982e76019c285d1c8c24086c0abc3078565615
JacobLinCool <jacoblin2011@gmail.com>
2019-03-22 23:58:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
 self.addEventListener('install', function(event){
     console.log('[Service Worker] Installed Service Worker!', event);
 });
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
