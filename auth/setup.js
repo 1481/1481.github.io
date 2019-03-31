a223a4804dcf55b025d04b36af983517539a3478
JacobLinCool <jacoblin2011@gmail.com>
2019-03-31 14:32:53 +0800

    Create setup.js

---
+++ auth/setup.js
@@ -0,0 +1,10 @@
+var config = {
+        apiKey: "AIzaSyAW060kfFPzaSKzflp9eXesmyn-GMYj-M8",
+        authDomain: "hsnu-1481-website.firebaseapp.com",
+        databaseURL: "https://hsnu-1481-website.firebaseio.com",
+        projectId: "hsnu-1481-website",
+        storageBucket: "hsnu-1481-website.appspot.com",
+        messagingSenderId: "405025421965"
+    };
+firebase.initializeApp(config);
+var db = firebase.firestore();
