43f8a01ad2c988e7f8cdc24f04a55f45cbc55de1
JacobLinCool <jacoblin2011@gmail.com>
2019-09-12 23:29:09 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -41,6 +41,7 @@ function GetWeather() {
         if(wx >= 9 && wx <= 11) {
             wi.classList.add("cloud-rain");
         }
+        wi.onclick = function(){Swal.fire(weather.records.locations[0].location[0].weatherElement[4].time[0].elementValue[0].value)};
         document.getElementsByClassName("TodayTime")[1].appendChild(wi);
         var temp = document.createElement("span");
         temp.innerHTML = " "+weather.records.locations[0].location[0].weatherElement[2].time[0].elementValue[0].value+"°C";
