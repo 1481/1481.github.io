bb99e7d93279a3b18d299c755030585390193da8
JacobLinCool <jacoblincool@gmail.com>
2019-11-06 00:29:03 +0800

    add service worker

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
 self.addEventListener('install', function(event){
     console.log('[Service Worker] Installed Service Worker!', event);
+    event.waitUntil(
+        caches.open('cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/icon.64.jpg?static=1',
+                '/icon.jpg?static=1',
+                '/css/w3.css?static=1',
+                '/css/materialdesignicons.min.css?static=1'
+            ]);
+        })
+    );
 });
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                    // Cache hit - return response
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request);
-                }
-            )
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
     );
 });
