57a30bdaab0a46450d05adfb77e75a25d1d175b4
JacobLinCool <jacoblincool@gmail.com>
2019-11-06 08:27:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
-self.addEventListener('install', function(event){
+self.addEventListener('install', event => {
     console.log('[Service Worker] Installed Service Worker!', event);
     event.waitUntil(
-        caches.open('cache').then(function(cache) {
+        caches.open('cache')
+        .then(cache => {
             return cache.addAll([
                 '/',
                 '/icon.64.jpg?static=1',
                 '/icon.jpg?static=1',
                 '/css/w3.css?static=1',
-                '/css/materialdesignicons.min.css?static=1'
+                '/css/materialdesignicons.min.css?static=1',
+                '/fonts/materialdesignicons-webfont.woff2?static=1'
             ]);
         })
+        .then(console.log)
     );
 });
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );
