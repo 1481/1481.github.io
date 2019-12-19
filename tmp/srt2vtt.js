5f3520700b26bf17af0d23d48e59a7eb6f797c08
JacobLinCool <jacoblin2011@gmail.com>
2019-12-20 07:08:42 +0800

    Create srt2vtt.js

---
+++ tmp/srt2vtt.js
@@ -0,0 +1,15 @@
+var srt = "SRT";
+var vtt = "WEBVTT \n\n";
+
+var split = srt.split("\n\n");
+for(var i = 0; i < split.length; i++) {
+    try {
+        var part = split[i].split("\n");
+        vtt += part[1].split(",").join(".") + "\n";
+        for(var j = 2; j < part.length; j++) {
+            vtt += part[j] + "\n";
+        }
+        vtt += "\n";
+    }
+    catch(e) {}
+}
