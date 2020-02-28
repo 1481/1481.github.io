50ac3b396e6a08f8d3f5f880a3b1ba68e4eac3dd
JacobLinCool <jacoblincool@gmail.com>
2020-02-28 16:49:12 +0800

    Update Service Worker

--- sw.js
+++ sw.js
@@ -1,25 +1,46 @@
-self.addEventListener('install', event => {
-    self.skipWaiting();
-    console.log('[Service Worker] Installed Service Worker!', event);
-    event.waitUntil(
-        caches.open('cache')
-        .then(cache => {
-            return cache.addAll([
-                '/',
-                '/icon.64.jpg?static=1',
-                '/icon.jpg?static=1',
-                '/css/w3.css?static=1',
-                '/css/materialdesignicons.min.css?static=1',
-                '/fonts/materialdesignicons-webfont.woff2?static=1'
-            ]);
-        })
-        .then(console.log)
-    );
+var CACHE = 'cache-2020-1';
+var assets = [
+	'/',
+	'/icon.64.jpg?static=1',
+	'/icon.jpg?static=1',
+	'/css/w3.css?static=1',
+	'/css/materialdesignicons.min.css?static=1',
+	'/fonts/materialdesignicons-webfont.woff2?static=1'
+];
+
+self.addEventListener('install', function(evt) {
+  console.log('[Service Worker] Installed.');
+  
+  evt.waitUntil(precache());
 });
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+
+self.addEventListener('fetch', function(evt) {
+  console.log('[Service Worker] Serving Asset. ', evt.request);
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(update(evt.request));
 });
+
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(assets);
+  }).then(() => {
+	  console.log('[Service Worker] Precached Assets.');
+	  console.log('[Service Worker] Assets: ', asset);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || fetch(request);
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
