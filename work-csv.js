04348622fec753b3f3eb72d8d8544866eca88453
JacobLinCool <jacoblincool@gmail.com>
2020-02-28 17:13:41 +0800

    using web worker

---
+++ work-csv.js
@@ -0,0 +1,20 @@
+onmessage = function(e) {
+  var data = e.data;
+  console.log('[CSV Maker] Received Data.', data);
+  console.log('[CSV Maker] Strat Encoding Proccess.');
+  
+  var csv = `\ufeff"座號","總計","未搬椅子","未打掃",`+data.dates.join(",")+"\n";
+  for(var i = 1; i < 30; i++) {
+	if(data.sname[i]) {
+	  csv += `${i},${data.total[i] ? data.total[i] : ''},${data.total_s[i] ? data.total_s[i] : ''},${data.total_j[i] ? data.total_j[i] : ''},`;
+	  for(var j = 0; j < data.dates_code.length; j++) {
+		csv += `"${data.data[dates_code[j]].seats ? (data.data[dates_code[j]].seats.includes(i) ? "X" : "") : ""}",`;
+		csv += `"${data.data[dates_code[j]].jobs ? (data.data[dates_code[j]].jobs.includes(i) ? "X" : "") : ""}",`;
+	  }
+	  csv += "\n";
+	}
+  }
+  
+  console.log('[CSV Maker] CSV File Encoded.');
+  postMessage(csv);
+}
