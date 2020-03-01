7253c5455b3676368a80072847feb9cef0a7e10b
JacobLinCool <jacoblincool@gmail.com>
2020-03-01 17:39:21 +0800

    Update work-csv.js

--- work-csv.js
+++ work-csv.js
@@ -8,8 +8,8 @@ onmessage = function(e) {
 	if(data.sname[i]) {
 	  csv += `${i},${data.total[i] ? data.total[i] : ''},${data.total_s[i] ? data.total_s[i] : ''},${data.total_j[i] ? data.total_j[i] : ''},`;
 	  for(var j = 0; j < data.dates_code.length; j++) {
-		csv += `"${data.data[dates_code[j]].seats ? (data.data[dates_code[j]].seats.includes(i) ? "X" : "") : ""}",`;
-		csv += `"${data.data[dates_code[j]].jobs ? (data.data[dates_code[j]].jobs.includes(i) ? "X" : "") : ""}",`;
+		csv += `"${data.data[data.dates_code[j]].seats ? (data.data[data.dates_code[j]].seats.includes(i) ? "X" : "") : ""}",`;
+		csv += `"${data.data[data.dates_code[j]].jobs ? (data.data[data.dates_code[j]].jobs.includes(i) ? "X" : "") : ""}",`;
 	  }
 	  csv += "\n";
 	}
